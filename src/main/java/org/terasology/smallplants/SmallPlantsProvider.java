f29a307246c62393bcd3c63926a0e8afed0b4d73
Scott M Sunarto <smsunarto@yahoo.com>
2017-01-08 20:53:27 +0700

    Initial commit

---
+++ src/main/java/org/terasology/smallplants/SmallPlantsProvider.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2016 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.terasology.smallplants;
+
+import org.terasology.math.geom.BaseVector2i;
+import org.terasology.utilities.procedural.Noise;
+import org.terasology.utilities.procedural.WhiteNoise;
+import org.terasology.world.generation.*;
+import org.terasology.world.generation.facets.SurfaceHeightFacet;
+import org.terasology.world.generator.plugin.RegisterPlugin;
+
+@RegisterPlugin
+@Produces(SmallPlantsFacet.class)
+@Requires(@Facet(SurfaceHeightFacet.class))
+public class HerbalismProvider implements FacetProviderPlugin {
+    private Noise noise;
+
+    @Override
+    public void setSeed(long seed) {
+        noise = new WhiteNoise(seed);
+    }
+
+    @Override
+    public void process(GeneratingRegion region) {
+        Border3D border = region.getBorderForFacet(SmallPlantsFacet.class);
+        SmallPlantsFacet facet = new SmallPlantsFacet(region.getRegion(), border);
+        SurfaceHeightFacet surfaceHeightFacet = region.getRegionFacet(SurfaceHeightFacet.class);
+
+        for (BaseVector2i position: surfaceHeightFacet.getWorldRegion().contents()) {
+            int surfaceHeight = (int) surfaceHeightFacet.getWorld(position);
+
+            if (facet.getWorldRegion().encompasses(position.x(), surfaceHeight + 1, position.y())
+                    && noise.noise(position.x(), position.y()) > 0.99) {
+                facet.setWorld(position.x(), surfaceHeight + 1, position.y(), true);
+            }
+        }
+
+        region.setRegionFacet(SmallPlantsFacet.class, facet);
+    }
+}
---
+++ src/main/java/org/terasology/smallplants/SmallPlantsRasterizer.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.smallplants;
+
+import org.terasology.math.ChunkMath;
+import org.terasology.math.geom.Vector3i;
+import org.terasology.registry.CoreRegistry;
+import org.terasology.world.block.Block;
+import org.terasology.world.block.BlockManager;
+import org.terasology.world.chunks.CoreChunk;
+import org.terasology.world.generation.Region;
+import org.terasology.world.generation.WorldRasterizerPlugin;
+import org.terasology.world.generator.plugin.RegisterPlugin;
+
+import java.util.Random;
+
+@RegisterPlugin
+public class HerbalismRasterizer implements WorldRasterizerPlugin {
+    private Block plant;
+    private String[] plantList;
+    private Random random;
+
+    @Override
+    public void initialize() {
+        random = new Random();
+        plantList = new String[]{"core:TallGrass1", "core:TallGrass2", "core:TallGrass3", "core:Dandelion", "core:BrownShroom", "Alchemy:RegenHerb", "Alchemy:HealingHerb"};
+    }
+
+    @Override
+    public void generateChunk(CoreChunk chunk, Region chunkRegion) {
+        SmallPlantsFacet herbFacet = chunkRegion.getFacet(SmallPlantsFacet.class);
+        for (Vector3i position: chunkRegion.getRegion()) {
+            if (herbFacet.getWorld(position)) {
+                int index =  random.nextInt(plantList.length);
+                plant = CoreRegistry.get(BlockManager.class).getBlock(plantList[index]);
+                chunk.setBlock(ChunkMath.calcBlockPos(position), plant);
+            }
+        }
+    }
+}
